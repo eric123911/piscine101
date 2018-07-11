113c4ebaa7a854a95e8d40cc032dae3e9f4aca2e
Eric leo william Schneller <eschnell@z1r12p4.le-101.fr>
2018-07-11 20:18:26 +0200

    new day

---
+++ dossier/ex00/ft_strdup.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strdup.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: eschnell <eschnell@student.le-101.>        +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/07/11 15:42:56 by eschnell     #+#   ##    ##    #+#       */
+/*   Updated: 2018/07/11 19:05:24 by eschnell    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int			ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char		*ft_strdup(char *src)
+{
+	int		i;
+	char	*cpy;
+
+	i = 0;
+	cpy = (char *)malloc(sizeof(*src) * (ft_strlen(src) + 1));
+	while (src[i])
+	{
+		cpy[i] = src[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
